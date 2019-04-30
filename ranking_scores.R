624934ef3de3e52d209e66fa7555decffe6cc610
Hanna Rudakouskaya <hannarud@mail.ru>
2019-04-30 20:24:03 +0300

    Upd

--- ranking_scores.R
+++ ranking_scores.R
@@ -58,8 +58,8 @@ ranking_scores <- function(results_source = c("googlesheets", "local"),
   results$`ФИ` <- str_replace_all(results$`ФИ`, c("ё" = "е", "Ё" = "Е"))
   
   # Убираем иностранных спортсменов
-  results <- results[!grepl("(Ukr)|(Rus)|(RUS)|(rus)|(Mda)|(Lat)|(Est)|(EST)$", results$`ФИ`), ]
-  results <- results[!grepl("(Rus)|(RUS)|(rus)|(EST)$", results$`Коллектив`), ]
+  results <- results[!grepl("(Ukr)|(Rus)|(RUS)|(rus)|(Mda)|(Lat)|(Est)|(EST)|(UKR)|(ukr)|(HUN)|(hun)|(FIN)|(POL)$", results$`ФИ`), ]
+  results <- results[!grepl("(Ukr)|(Rus)|(RUS)|(rus)|(Mda)|(Lat)|(Est)|(EST)|(UKR)|(ukr)|(HUN)|(hun)|(FIN)|(POL)$", results$`Коллектив`), ]
   
   # У сошедших в графе "место" стоит 0, у участников в/к - "в/к"
   results$`Место`[results$`Место` == "в/к"] <- "0"
