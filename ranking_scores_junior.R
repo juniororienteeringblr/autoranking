7f7815ef966275c4735dedcb5283c292c4f1be9e
Hanna Rudakouskaya <Hanna.Rudakouskaya@wdc.com>
2021-12-20 10:34:27 +0100

    Юниорский рейтинг обновлен для 2021 года. Все еще плохо сделан подсчет очков, остальное норм

--- ranking_scores_junior.R
+++ ranking_scores_junior.R
@@ -1,7 +1,7 @@
 ranking_scores_junior <- function(competition_date = NA,
                            competition_name = NA,
                            competition_distance = NA,
-                           ranking_type = c("youth", "junior"),
+                           ranking_type = c("junior"),
                            competition_coefficient = NA) {
   # Шапка файлов результатов должна быть следующей
   # ФИ;Коллектив;Квал;Номер;ГР;Результат;Место;Прим;Группа
@@ -9,6 +9,8 @@ ranking_scores_junior <- function(competition_date = NA,
   print(paste("Computing ranking for date:", competition_date, "and coefficient", competition_coefficient))
 
   require(googlesheets4)
+  library(stringi)
+  library(stringr)
   
   # Ищем тот документ, который соответствует дате
   print(paste("Searching for the file", paste0(competition_date, "_", competition_name, "_", competition_distance)))
@@ -24,13 +26,23 @@ ranking_scores_junior <- function(competition_date = NA,
   reference_database <- as.data.frame(read_sheet(drive_find(pattern = "Orienteers database",
                                                             type = "spreadsheet", n_max=1),
                                                  sheet = format(Sys.Date(), "%Y"),
-                                                 col_types='ccciccccccccc'))
+                                                 col_types='cccicccccccc'))
   
-  library(stringi)
-  library(stringr)
   results$`ФИ` <- str_to_title(results$`ФИ`)
-  results$`ФИ` <- str_replace_all(results$`ФИ`, stri_enc_toutf8("ё"), stri_enc_toutf8("е"))
-  results$`ФИ` <- str_replace_all(results$`ФИ`, stri_enc_toutf8("Ё"), stri_enc_toutf8("Е"))
+  results$`ФИ` <- str_replace_all(results$`ФИ`,
+                                  stri_enc_toutf8("\U0451"), # ё
+                                  stri_enc_toutf8("\U0435")) # е
+  results$`ФИ` <- str_replace_all(results$`ФИ`,
+                                  stri_enc_toutf8("\U0401"), # Ё
+                                  stri_enc_toutf8("\U0415")) # Е
+  
+  reference_database$`ФИ` <- str_to_title(reference_database$`ФИ`)
+  reference_database$`ФИ` <- str_replace_all(reference_database$`ФИ`,
+                                             stri_enc_toutf8("\U0451"), # ё
+                                             stri_enc_toutf8("\U0435")) # е
+  reference_database$`ФИ` <- str_replace_all(reference_database$`ФИ`,
+                                             stri_enc_toutf8("\U0401"), # Ё
+                                             stri_enc_toutf8("\U0415")) # Е
   
   # # Убираем иностранных спортсменов
   # results <- results[!grepl("(Ukr)|(Rus)|(RUS)|(rus)|(Mda)|(Lat)|(Est)|(EST)|(UKR)|(ukr)|(HUN)|(hun)|(FIN)|(POL)|(pol)$", results$`ФИ`), ]
@@ -38,7 +50,7 @@ ranking_scores_junior <- function(competition_date = NA,
   
   library(dplyr)
   # Убираем спортсменов-не членов федерации
-  reference_database <- reference_database[! is.na(reference_database$`Член БФО в текущем году`), ]
+  reference_database <- reference_database[! is.na(reference_database$`Членство БФО в текущем году`), ]
   # Убираем тех, кто оплатил взнос после даты соревнований
   reference_database <- reference_database[as.Date(reference_database$`Дата оплаты`, '%d.%m.%Y') <= as.Date(competition_date, '%Y%m%d'), ]
   filtered_out <- anti_join(results, reference_database, by = c("ФИ", "ГР"))
@@ -49,17 +61,13 @@ ranking_scores_junior <- function(competition_date = NA,
   results$`Место` <- as.numeric(results$`Место`)
   results$`Результат_сек` <- as.numeric(as.difftime(as.character(results$`Результат`), format = "%H:%M:%S", units = "secs"))
   
-  # coefs_group <- read.csv2(file = "group_coef.txt", encoding = "UTF-8", stringsAsFactors = FALSE,
-  #                          colClasses = c("character", "double"))
-  
-  # results$`Сложность` <- substr(results$`Группа`, str_locate(results$`Группа`, paste0("(", paste0(coefs_group$`Сложность`, collapse = "|"), ")")), 1000)
   results$`Сложность` <- substr(results$`Группа`, 2, 1000)
   # Выбираем только группу 20 и сильнейшую из элитных групп
   # ЕЩЕ НЕ СДЕЛАНО, СДЕЛАТЬ!!!
-  results <- results %>% filter(`Сложность` == stri_enc_toutf8("20") |
-                                  `Сложность` == stri_enc_toutf8("21") |
-                                  `Сложность` == stri_enc_toutf8("21E") |
-                                  `Сложность` == stri_enc_toutf8("21Е"))
+  results <- results %>% filter(`Сложность` == stri_enc_toutf8("\u0032\u0030") | # 20
+                                  `Сложность` == stri_enc_toutf8("\u0032\u0031") | # 21
+                                  `Сложность` == stri_enc_toutf8("\u0032\u0031\u0415") | # 21Е
+                                  `Сложность` == stri_enc_toutf8("\u0032\u0031\u0045")) # 21E
   
   library(dplyr)
   # results <- left_join(results, coefs_group, by = c("Сложность" = "Сложность"))
@@ -77,9 +85,6 @@ ranking_scores_junior <- function(competition_date = NA,
   results$`Очки`[results$`Очки` < 1] <- 0
   results$`Очки`[is.na(results$`Очки`)] <- 0
   results$`Очки`[results$`Место` == 0] <- 0
-  
-  names(results) <- stri_enc_toutf8(names(results), is_unknown_8bit = FALSE, validate = FALSE)
-  names(filtered_out) <- stri_enc_toutf8(names(filtered_out), is_unknown_8bit = FALSE, validate = FALSE)
   
   results <- as.data.frame(results)
   filtered_out <- as.data.frame(filtered_out)
